ab8110dd08c90e6274ad7b1633992785a26366a1
danilopenia <danilopenia14@gmail.com>
2023-10-20 19:18:24 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import Navbar from "./componentes/Navbar"
 import ItemListContainer from "./componentes/ItemListContainer"
+import Componente from './componentes/ComponenteA'
+import Componente from './componentes/ComponenteB'
 
 
 const App = () => {
+  const tomarValor = (valor)=>{
+    console.log(`valor: ${valor}`);
+  }
+
+  const user={
+    nombre: "coder",
+    apellido:"House",
+    edad:50
+  }
+
+  const arr = ["A", "B", "C"]
+
   return (
     <div>
       <Navbar></Navbar>
       <ItemListContainer greeting={"bienvenido a mi ecomerce"}/>
+      <Componente tomarValor={tomarValor} isAdmin={false} user={user} arr={arr}/>
     </div>
   )
 }
